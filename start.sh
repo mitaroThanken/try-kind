803c58d7d669d1da96d2a2351219329082955bca
YAMAMI Taro <mitarothanken@gmail.com>
2022-09-10 22:07:19 +0900

    add startup script (with Web UI)

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+WEB_UI_VERSION=v2.6.1
+
+# create cluster
+kind create cluster
+
+# trap
+trap "kind delete cluster" SIGINT
+
+# Web UI with sample user
+kubectl apply -f "https://raw.githubusercontent.com/kubernetes/dashboard/${WEB_UI_VERSION}/aio/deploy/recommended.yaml"
+kubectl apply -f ./web-ui-sample-user.yaml
+echo "Web UI starting..."
+kubectl wait deployment -n kubernetes-dashboard kubernetes-dashboard --for condition=Available=True --timeout=2m
+
+# Started
+echo "Web UI access token"
+kubectl -n kubernetes-dashboard create token admin-user
+
+echo "Dashboard available at http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
+
+echo 'CTRL-C to delete cluster'
+kubectl proxy
