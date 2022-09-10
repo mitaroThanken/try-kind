343c815cb35b346937fad1b634c3617c8fc83b5b
YAMAMI Taro <mitarothanken@gmail.com>
2022-09-11 00:40:19 +0900

    add Contour

--- start.sh
+++ start.sh
@@ -9,6 +9,12 @@ kind create cluster --config=config-cluster.yaml
 # trap
 trap "kind delete cluster" SIGINT
 
+# contour
+kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
+echo "Contour starting..."
+kubectl wait deployment -n projectcontour contour --for condition=Available=True --timeout=2m
+kubectl patch daemonsets -n projectcontour envoy -p '{"spec":{"template":{"spec":{"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/control-plane","operator":"Equal","effect":"NoSchedule"},{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
+
 # Web UI with sample user
 kubectl apply -f "https://raw.githubusercontent.com/kubernetes/dashboard/${WEB_UI_VERSION}/aio/deploy/recommended.yaml"
 kubectl apply -f ./web-ui-sample-user.yaml
