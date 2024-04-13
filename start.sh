c4c42281eeb9256e496d13f3fb535b0b5ec7246a
YAMAMI Taro <mitarothanken@gmail.com>
2024-04-13 15:49:39 +0900

    update script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-METALLB_VERSION=v0.13.5
+METALLB_VERSION=v0.14.4
 
 # Create cluster
 kind create cluster --config=config-cluster.yaml
@@ -29,9 +29,19 @@ kubectl apply -f ./web-ui-sample-user.yaml
 
 # Wait for available
 echo "MetalLB starting..."
-kubectl wait deployment -n metallb-system controller --for condition=Available=True --timeout=2m
+kubectl wait --namespace metallb-system \
+                --for=condition=ready pod \
+                --selector=app=metallb \
+                --timeout=90s
 echo "Web UI starting..."
-kubectl wait deployment -n kubernetes-dashboard kubernetes-dashboard-kong --for condition=Available=True --timeout=1m
+kubectl wait --namespace kubernetes-dashboard \
+                --for=condition=ready pod \
+                --selector=app.kubernetes.io/instance=kubernetes-dashboard \
+                --timeout=90s
+
+# Setup address pool used by loadbalancers
+# https://kind.sigs.k8s.io/docs/user/loadbalancer/#setup-address-pool-used-by-loadbalancers
+kubectl apply -f ./metallb-config.yaml
 
 # Getting a Bearer Token for ServiceAccount
 # https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md#getting-a-bearer-token-for-serviceaccount
