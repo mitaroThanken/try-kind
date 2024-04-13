b00b29b8f1b80a6190b87dcb2545c5e1a816337a
YAMAMI Taro <mitarothanken@gmail.com>
2024-04-13 17:37:03 +0900

    modify wait

--- start.sh
+++ start.sh
@@ -23,9 +23,23 @@ kubectl apply -f - -n kube-system
 helm repo add metallb https://metallb.github.io/metallb
 helm upgrade --install metallb metallb/metallb --create-namespace --namespace metallb-system
 
+# Wait for available
+echo "MetalLB starting..."
+kubectl wait --namespace metallb-system \
+                --for=condition=ready pod \
+                --selector=app.kubernetes.io/instance=metallb \
+                --timeout=2m
+
 # Install istio
 istioctl install --set profile=demo -y
 kubectl label namespace default istio-injection=enabled
+
+# Wait for available
+echo "Istio starting..."
+kubectl wait --namespace istio-system \
+                --for=condition=ready pod \
+                --selector=release=istio \
+                --timeout=2m
 
 # Install dashboard
 # https://github.com/kubernetes/dashboard/tree/master?tab=readme-ov-file#installation
@@ -40,16 +54,11 @@ helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dash
 kubectl apply -f ./web-ui-sample-user.yaml
 
 # Wait for available
-echo "MetalLB starting..."
-kubectl wait --namespace metallb-system \
-                --for=condition=ready pod \
-                --selector=app.kubernetes.io/instance=metallb \
-                --timeout=90s
 echo "Web UI starting..."
 kubectl wait --namespace kubernetes-dashboard \
                 --for=condition=ready pod \
                 --selector=app.kubernetes.io/instance=kubernetes-dashboard \
-                --timeout=90s
+                --timeout=2m
 
 # Setup address pool used by loadbalancers
 # https://kind.sigs.k8s.io/docs/user/loadbalancer/#setup-address-pool-used-by-loadbalancers
